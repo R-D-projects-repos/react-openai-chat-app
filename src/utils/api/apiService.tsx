da0fffe2f6c2955239c7281248957365c9550863
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2023-05-01 18:07:33 +0500

    done

---
+++ src/utils/api/apiService.tsx
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const fetchData = async (input: string) => {
+  const model = 'text-davinci-002';
+  const response = await axios.post(
+    'https://api.openai.com/v1/completions',
+    {
+      prompt: `Complete this sentence: "${input}"`,
+      model: model,
+      max_tokens: 50,
+      n: 1,
+      stop: '.',
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      },
+    }
+  );
+
+  return response.data.choices[0].text;
+};
